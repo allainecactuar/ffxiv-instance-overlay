61afc68ed9fa787527831e536c6708dffb180d45
Allaine Cactuar <allainecactuar@gmail.com>
2021-12-06 11:41:29 -0800

    Updates

--- instance-overlay.js
+++ instance-overlay.js
@@ -2,9 +2,13 @@
 var InstanceOverlay;
 (function (InstanceOverlay) {
     var LOG_PARSE_EVENT = 'onLogLine';
-    var CHANGE_ZONE_OPCODE = 1;
-    var LOG_LINE_OPCODE = 0;
-    var INSTANCE_MESSAGE_ID = '0039';
+    var LINE_TYPE_INDEX = 0;
+    var TIMESTAMP_INDEX = 1;
+    var LINE_TYPE_LOGLINE = '00';
+    var LOGLINE_CODE_INDEX = 2;
+    var LOGLINE_LINE_INDEX = 4;
+    var LINE_TYPE_CHANGEZONE = '01';
+    var INSTANCE_MESSAGE_LOGLINE_CODE = '0039';
     var INSTANCE_MESSAGE_PATTERN = /You are now in the instanced area (.*?)(.)[.]/;
     var INSTANCE_MESSAGE_AREA_NAME_INDEX = 1;
     var INSTANCE_MESSAGE_INSTANCE_SYMBOL_INDEX = 2;
@@ -23,20 +27,27 @@ var InstanceOverlay;
     }
     function onLogLine(event) {
         if (typeof event !== 'object' ||
-            typeof event.detail !== 'object' ||
-            !Array.isArray(event.detail.payload)) {
-            console.log('Bad event object to onLogLine');
+            typeof event.detail !== 'string') {
+            console.log("Bad event object to onLogLine: " + event.detail);
             return;
         }
-        if (CHANGE_ZONE_OPCODE === event.detail.opcode) {
+        var detail = JSON.parse(event.detail);
+        if (!Array.isArray(detail) ||
+            detail.length < LINE_TYPE_INDEX + 1) {
+            console.log("Bad event object to onLogLine: " + event.detail);
+            return;
+        }
+        var lineType = detail[LINE_TYPE_INDEX];
+        if (LINE_TYPE_CHANGEZONE === lineType) {
             setInstance(0);
             return;
         }
-        if (LOG_LINE_OPCODE !== event.detail.opcode ||
-            INSTANCE_MESSAGE_ID !== event.detail.payload[0]) {
+        if (LINE_TYPE_LOGLINE !== lineType ||
+            detail.length < LOGLINE_LINE_INDEX + 1 ||
+            INSTANCE_MESSAGE_LOGLINE_CODE !== detail[LOGLINE_CODE_INDEX]) {
             return;
         }
-        var logLineMessage = event.detail.payload[2];
+        var logLineMessage = detail[LOGLINE_LINE_INDEX];
         var match = logLineMessage.match(INSTANCE_MESSAGE_PATTERN);
         if (null === match) {
             return;
